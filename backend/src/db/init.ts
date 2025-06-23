50589e5fff1b41169fb5af77010a83170eda936d
Brian Nhan <briannnhan@gmail.com>
2025-06-23 18:38:27 +1000

    feat(db migration): attempted bugfix for kysley migration

--- backend/src/db/init.ts
+++ backend/src/db/init.ts
@@ -1,34 +1,59 @@
-// backend/src/db/init.ts
-import { CamelCasePlugin, Kysely, PostgresDialect } from 'kysely';
-import pg from 'pg';
+import * as path from 'path';
 import dotenv from 'dotenv';
-import { up } from './migrations';
+import { Pool } from 'pg';
+import { promises as fs } from 'fs';
+import {
+  Kysely,
+  Migrator,
+  PostgresDialect,
+  FileMigrationProvider,
+  CamelCasePlugin,
+} from 'kysely';
 
 // Load env vars from .env
 dotenv.config();
 
-const db = new Kysely<any>({
-  dialect: new PostgresDialect({
-    pool: new pg.Pool({
-      host: process.env.DB_HOST,
-      port: Number(process.env.DB_PORT),
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
+async function migrateToLatest() {
+  const db = new Kysely<any>({
+    dialect: new PostgresDialect({
+      pool: new Pool({
+        host: process.env.DB_HOST,
+        port: Number(process.env.DB_PORT),
+        database: process.env.DB_NAME,
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+      }),
     }),
-  }),
-  plugins: [new CamelCasePlugin()],
-});
+    plugins: [new CamelCasePlugin()],
+  });
 
-async function migrate() {
-  try {
-    await up(db);
-    console.log('Migration completed');
-  } catch (err) {
-    console.error('Migration failed:', err);
-  } finally {
-    await db.destroy();
+  const migrator = new Migrator({
+    db,
+    provider: new FileMigrationProvider({
+      fs,
+      path,
+      // This needs to be an absolute path.
+      migrationFolder: path.join(__dirname, './migrations'),
+    }),
+  });
+
+  const { error, results } = await migrator.migrateToLatest();
+
+  results?.forEach((it) => {
+    if (it.status === 'Success') {
+      console.log(`migration "${it.migrationName}" was executed successfully`);
+    } else if (it.status === 'Error') {
+      console.error(`failed to execute migration "${it.migrationName}"`);
+    }
+  });
+
+  if (error) {
+    console.error('failed to migrate');
+    console.error(error);
+    process.exit(1);
   }
+
+  await db.destroy();
 }
 
-migrate();
+migrateToLatest();
