6724135c66d2beea212c6fb94e6cfb5d0ecf9430
Brian Nhan <briannnhan@gmail.com>
2025-07-01 21:27:52 +1000

    refactor(db): updated types to match new schema

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,12 +1,12 @@
 import { Kysely, PostgresDialect, CamelCasePlugin } from 'kysely';
-// import { DB } from 'kysely-codegen';
+import { DB } from 'kysely-codegen';
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
 // Load env vars from .env
 dotenv.config();
 
-export const db = new Kysely<any>({
+export const db = new Kysely<DB>({
   dialect: new PostgresDialect({
     pool: new Pool({
       connectionString: process.env.DATABASE_URL,
