baf4878caaaab4e3c9f98222d01afaaeed277217
jeremyle56 <jeremyle420@gmail.com>
2025-07-13 23:44:09 +1000

    feat(client): add test to env api config

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -1,10 +1,12 @@
 export enum Env {
   DEV = 'development',
+  TEST = 'test',
   PROD = 'production',
 }
 
 const API_CONFIG: Record<string, string> = Object.freeze({
   [Env.DEV]: 'http://localhost:3001/api',
+  [Env.TEST]: 'http://localhost:3001/api',
   //   [Env.PROD]: 'TODO:',
 });
 
