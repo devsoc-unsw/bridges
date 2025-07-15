360a6aa848c71a6c456350a08701151caa8740fd
jeremyle56 <jeremyle420@gmail.com>
2025-07-16 01:42:57 +1000

    feat(client): add auth to frontend routes

--- client/next.config.ts
+++ client/next.config.ts
@@ -1,7 +1,15 @@
+import dotenv from 'dotenv';
 import type { NextConfig } from 'next';
+
+dotenv.config({ path: '../env/client.env' });
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  env: {
+    AUTH_SECRET: process.env.AUTH_SECRET,
+    AUTH_GOOGLE_ID: process.env.AUTH_GOOGLE_ID,
+    AUTH_GOOGLE_SECRET: process.env.AUTH_GOOGLE_SECRET,
+  },
 };
 
 export default nextConfig;
--- client/src/app/login/page.tsx
+++ client/src/app/login/page.tsx
@@ -1,19 +1,34 @@
-'use client';
+import { auth, signIn, signOut } from '../../auth';
 
-import { useRouter } from 'next/navigation';
+export default async function HomePage() {
+  const session = await auth();
+  const user = session?.user;
 
-export default function HomePage() {
-  const router = useRouter();
-
-  const handleClick = () => {
-    router.push('/home');
-  };
-
-  return (
-    <div className="min-h-screen items-center justify-center flex">
-      <button onClick={handleClick} className="cursor-pointer rounded bg-blue-400 px-6 py-3 text-white shadow">
-        Sign in with Google!
-      </button>
+  return user ? (
+    <div className="flex min-h-screen items-center justify-center">
+      <form
+        action={async () => {
+          'use server';
+          await signOut();
+        }}
+      >
+        <button type="submit" className="cursor-pointer rounded bg-blue-400 px-6 py-3 text-white shadow">
+          Sign out :(
+        </button>
+      </form>
+    </div>
+  ) : (
+    <div className="flex min-h-screen items-center justify-center">
+      <form
+        action={async () => {
+          'use server';
+          await signIn('google', { redirectTo: '/protected' });
+        }}
+      >
+        <button type="submit" className="cursor-pointer rounded bg-blue-400 px-6 py-3 text-white shadow">
+          Sign in with Google!
+        </button>
+      </form>
     </div>
   );
 }
